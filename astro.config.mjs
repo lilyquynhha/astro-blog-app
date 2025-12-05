f97e48d5aaef8f74cf462aeb76e466d919b3f4db
Lily <haquynhchi@gmail.com>
2025-12-05 13:57:22 +1100

    Fixed site URL in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://app.netlify.com/projects/lily-ha-blog-app/configuration/general"
+  site: "https://lily-ha-blog-app.netlify.app/",
 });
